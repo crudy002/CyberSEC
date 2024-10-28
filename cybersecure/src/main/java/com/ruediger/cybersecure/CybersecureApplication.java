528a12226cc5a40712800cd766da665c38a9bc0b
Christopher Ruediger <christopher.b.ruediger@gmail.com>
2024-10-28 10:13:11 +0000

    added spring project

---
+++ cybersecure/src/main/java/com/ruediger/cybersecure/CybersecureApplication.java
@@ -0,0 +1,13 @@
+package com.ruediger.cybersecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybersecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CybersecureApplication.class, args);
+	}
+
+}
