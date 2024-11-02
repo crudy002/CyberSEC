869feeb34b727f26d2c1be4ab2f8ac671e9b1dc7
Christopher Ruediger <christopher.b.ruediger@gmail.com>
2024-11-02 19:15:00 +0000

    Added working spring project with an example product application

---
+++ cybersecure/src/test/java/com/ruediger/cybersecure/CybersecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruediger.cybersecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybersecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
