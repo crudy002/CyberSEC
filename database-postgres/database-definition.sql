c5b50c583ace431ba0602231c7e1f1fba340a3e7
Christopher Ruediger <christopher.b.ruediger@gmail.com>
2024-11-02 13:20:10 +0000

    Removed spring boot app for now, focusing on database, added in more ddl for database and staged nsert statments for control families

--- database-postgres/database-definition.sql
+++ database-postgres/database-definition.sql
@@ -1,44 +1,27 @@
 CREATE DATABASE compliance_tracker;
 
-
--- Systems Table
-CREATE TABLE systems (
-    system_id SERIAL PRIMARY KEY,
-    system_name VARCHAR(100) NOT NULL,
-    description TEXT,
-    owner VARCHAR(100),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE control_families (
+    family_id VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
 );
 
--- Controls Table
-CREATE TABLE controls (
-    control_id SERIAL PRIMARY KEY,
-    control_name VARCHAR(50) NOT NULL,
-    control_description TEXT,
-    control_type VARCHAR(50),
-    family VARCHAR(50),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+/*
+ALTER TABLE control_families
+DROP description;
 
--- Mappings Table
-CREATE TABLE system_control_mappings (
-    mapping_id SERIAL PRIMARY KEY,
-    system_id INT REFERENCES systems(system_id) ON DELETE CASCADE,
-    control_id INT REFERENCES controls(control_id) ON DELETE CASCADE,
-    implementation_status VARCHAR(50),
-    comments TEXT,
-    last_reviewed TIMESTAMP
-);
+DROP TABLE control_families;
 
--- Documentation Table
-CREATE TABLE documentation (
-    doc_id SERIAL PRIMARY KEY,
-    control_id INT REFERENCES controls(control_id) ON DELETE CASCADE,
-    system_id INT REFERENCES systems(system_id) ON DELETE CASCADE,
-    document_link VARCHAR(255) NOT NULL,
-    version INT DEFAULT 1,
-    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+INSERT INTO control_families (family_id, name)
+VALUES (
+    family_id:integer,
+    'name:character varying'
+  );
+
+COPY control_families (family_id, name)
+FROM 'database-postgres/control_families.csv' 
+DELIMITER ',' 
+CSV HEADER;
+*/
 
 # Command line
 # \l - list databases
